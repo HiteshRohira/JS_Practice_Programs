f38f3a87c1dc70c0600fb7ffce4fd62929aa2053
bop <hitesh@fedorabox.lan>
2020-08-16 23:52:26 +0530

    n/a

--- javascript.info/5. Advanced working with functions/9. Decorators/throttle.js
+++ javascript.info/5. Advanced working with functions/9. Decorators/throttle.js
@@ -1,9 +1,24 @@
 function throttle(func, time) {
-  return function() {
-    func.apply(this, arguments);
-    this.timerId = setTimeout(() => {}, time);
-    if (this.timerId == 0) {
-      func.apply(this, arguments);
+  let savedArgs, savedThis, timerGoing = false;
+
+  function wrapper() {
+    
+    if (timerGoing) {
+      savedArgs = arguments;
+      savedThis = this;
+      return;
     }
+
+    func.apply(this, arguments);
+    timerGoing = true;
+
+    setTimeout( () => {
+      timerGoing = false;
+      if(savedArgs) {
+        wrapper.apply(savedThis, savedArgs);
+      }
+    }, time );
   }
+
+  return wrapper;
 }
