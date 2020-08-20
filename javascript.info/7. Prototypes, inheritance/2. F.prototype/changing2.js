f47e23f5990f9c694db83400834b1bed6b696562
bop <bop@pop-os.localdomain>
2020-08-21 00:01:04 +0530

    n/a

--- javascript.info/7. Prototypes, inheritance/2. F.prototype/changing2.js
+++ javascript.info/7. Prototypes, inheritance/2. F.prototype/changing2.js
@@ -5,6 +5,18 @@ Rabbit.prototype = {
 
 let rabbit = new Rabbit();
 
-Rabbit.prototype.eats = false;
+Rabbit.prototype.eats = false; // ...(1)
 
-alert( rabbit.eats ); // ?
+/* 
+In the previous one, object in prototype property of Rabbit is also refrenced by rabbit object. Since we know that objects are truly removed when all refrences are erased and rabbit still had refrence to previous object, it had that while value while Rabbit.prototype moved on to new object.
+
+In this, the object in memory refrenced by both Rabbit.prototype and [[Prototype]] of rabbit. That same object's eats property is changed by statement labelled (1)
+
+The output proves this clearly.
+*/
+
+console.log( rabbit.eats );
+
+rabbit.eats = true;
+
+console.log( rabbit.eats );
