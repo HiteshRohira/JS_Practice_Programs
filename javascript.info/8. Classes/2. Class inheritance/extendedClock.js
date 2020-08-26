2028e7efd830297b99d86d0702ae42e120e82baa
bop <bop@pop-os.localdomain>
2020-08-26 16:57:53 +0530

    n/a

--- javascript.info/8. Classes/2. Class inheritance/extendedClock.js
+++ javascript.info/8. Classes/2. Class inheritance/extendedClock.js
@@ -34,10 +34,22 @@ class Clock {
 }
 
 class ExtendedClock extends Clock {
-  constructor({ template }, precision) {
-    super({ template });
+  constructor(options) {
+    super(options);
+    let { precision = 1000 } = options;
     this.precision = precision;
   }
 
-  
+  start() {
+    this.render();
+    this.timer = setInterval(() => this.render(), this.precision);
+  }
+
 }
+
+let lowResolutionClock = new ExtendedClock({
+  template: 'h:m:s',
+  precision: 10000
+});
+
+lowResolutionClock.start();
