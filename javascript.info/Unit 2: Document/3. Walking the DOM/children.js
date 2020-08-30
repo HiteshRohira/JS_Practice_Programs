0bb7be85af2957f3189de8d91f4be4878ce290d4
Neon <bop@pop-os.localdomain>
2020-08-30 17:20:47 +0530

    n/a

---
+++ javascript.info/Unit 2: Document/3. Walking the DOM/children.js
@@ -0,0 +1,8 @@
+// to access div element
+document.body.firstElementChild;
+
+// to access ul element
+document.body.lastElementChild;
+
+//to access li element
+document.body.lastElementChild.lastElementChild;
---
+++ javascript.info/Unit 2: Document/3. Walking the DOM/diagnolSelect.js
@@ -0,0 +1,5 @@
+let td;
+for (let i = 0; i < 5; i++) {
+  td = table.rows[i].cells[i];
+  td.style.backgroundColor = "red";
+}
