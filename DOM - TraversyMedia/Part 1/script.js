c1046ebc81c150f782a5c087e01fc1ab1954674f
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-09-05 16:56:25 +0530

    n/a

--- DOM - TraversyMedia/Part 1/script.js
+++ DOM - TraversyMedia/Part 1/script.js
@@ -1,5 +1,29 @@
-// EXAMINE THE DOCUMENT OBJECT //
+// EXAMINE THE DOCUMENT OBJECT
 
 // console.dir(document);
-console.log(document.domain);
-console.log(document.URL);
+// console.log(document.domain);
+// console.log(document.URL);
+// console.log(document.title);
+// // document.title = 123;
+// console.log(document.doctype);
+// console.log(document.head);
+// console.log(document.body);
+// console.log(document.all);
+// console.log(document.all[10]);
+// console.log(document.forms);
+// console.log(document.images);
+// console.log(document.links);
+
+
+// GET ELEMENT BY ID
+// console.log(document.getElementById("header-title"));
+let headerTitle = document.getElementById("header-title");
+let header = document.getElementById("main-header");
+// console.log(headerTitle);
+// headerTitle.textContent = "Hello";
+// headerTitle.innerHTML = "Goodbye";
+// console.log(headerTitle.innerText);
+// headerTitle.innerHTML = "<h3>Hello</h3>";
+// header.style.borderBottom = "solid 4px black";
+
+// GET ELEMENTS BY CLASSNAME
