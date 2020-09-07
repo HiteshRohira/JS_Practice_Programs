7bb9ed53ff0ca23ea1febf0e0ab640c3b5d4c45c
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-09-07 18:17:08 +0530

    n/a

--- DOM - TraversyMedia/Part 1/script.js
+++ DOM - TraversyMedia/Part 1/script.js
@@ -204,9 +204,40 @@ let box = document.querySelector("#box");
 // box.addEventListener("mouseenter", runEvent);
 // box.addEventListener("mouseleave", runEvent);
 
-box.addEventListener("mouseover", runEvent);
-box.addEventListener("mouseout", runEvent);
+// box.addEventListener("mouseover", runEvent);
+// box.addEventListener("mouseout", runEvent);
+
+// box.addEventListener("mousemove", runEvent);
+
+let itemInput = document.querySelector(`input[type="text"]`);
+let form = document.querySelector("form");
+let select = document.querySelector("select");
+
+// itemInput.addEventListener("keydown", runEvent);
+// itemInput.addEventListener("keyup", runEvent);
+// itemInput.addEventListener("keypress", runEvent);
+
+// itemInput.addEventListener("focus", runEvent);
+// itemInput.addEventListener("blur", runEvent);
+
+// itemInput.addEventListener("cut", runEvent);
+// itemInput.addEventListener("paste", runEvent);
+
+// itemInput.addEventListener("input", runEvent);
+
+// select.addEventListener("change", runEvent);
+// select.addEventListener("input", runEvent);
+
+form.addEventListener("submit", runEvent);
 
 function runEvent(e) {
+  e.preventDefault();
   console.log("EVENT TYPE: "+e.type);
+
+  // console.log(e.target.value);
+  // output.innerHTML = `<h3>${e.target.value}</h3>`;
+  
+  // output.innerHTML = `<h3>Mouse Coordinates: (${e.offsetX}, ${e.offsetY})</h3>`;
+
+  // document.body.style.backgroundColor = `rgb(${e.offsetX}, ${e.offsetY}, 100)`;
 }
