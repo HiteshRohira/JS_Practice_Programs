6bb6940a736d88bbc3bd8ef57236e5187c4c9fcb
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-09-07 17:16:08 +0530

    n/a

--- DOM - TraversyMedia/Part 1/script.js
+++ DOM - TraversyMedia/Part 1/script.js
@@ -194,12 +194,18 @@
 // }
 
 let button = document.querySelector("#button");
+let box = document.querySelector("#box");
 
 // button.addEventListener("click", runEvent);
 // button.addEventListener("dblclick", runEvent);
 // button.addEventListener("mousedown", runEvent);
 // button.addEventListener("mouseup", runEvent);
 
+// box.addEventListener("mouseenter", runEvent);
+// box.addEventListener("mouseleave", runEvent);
+
+box.addEventListener("mouseover", runEvent);
+box.addEventListener("mouseout", runEvent);
 
 function runEvent(e) {
   console.log("EVENT TYPE: "+e.type);
