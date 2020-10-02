489f9b6e94964a29d2b67b260c2025975f44517c
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-03 01:06:52 +0530

    n/a

---
+++ MDN/4) Asynchronous JavaScript/Cooperative asynchronous Java​Script: Timeouts and intervals/Loading Animation/script.js
@@ -0,0 +1,22 @@
+const spinner = document.querySelector('div');
+let rotateCount = 0;
+let startTime = null;
+let rAF;
+
+function draw(timestamp) {
+	if(!startTime) {
+		startTime = timestamp;
+	}
+
+	rotateCount = (timestamp - startTime) / 3;
+
+	if(rotateCount > 359) {
+		rotateCount %= 360;
+	}
+
+	spinner.style.transform = 'rotate(' + rotateCount + 'deg)';
+
+	rAF = requestAnimationFrame(draw);
+}
+
+draw();
