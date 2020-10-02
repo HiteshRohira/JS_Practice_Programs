9cff0e8d3949f833d02f72cc8be9ec024ca2733f
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-03 01:15:08 +0530

    n/a

--- MDN/4) Asynchronous JavaScript/Cooperative asynchronous Java​Script: Timeouts and intervals/Loading Animation/script.js
+++ MDN/4) Asynchronous JavaScript/Cooperative asynchronous Java​Script: Timeouts and intervals/Loading Animation/script.js
@@ -2,6 +2,18 @@ const spinner = document.querySelector('div');
 let rotateCount = 0;
 let startTime = null;
 let rAF;
+let spinning = true;
+
+document.body.addEventListener("click", () => {
+	if (spinning === true) {
+		cancelAnimationFrame(rAF);
+		spinning = false;
+	}
+	else {
+		draw();
+		spinning = true;
+	}
+});
 
 function draw(timestamp) {
 	if(!startTime) {
