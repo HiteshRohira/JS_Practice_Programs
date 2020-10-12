2681a701620909467fa2a385635b50dd411bbe69
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-12 14:20:08 +0530

    n/a

--- ReactFrontToBack/github-finder/src/App.js
+++ ReactFrontToBack/github-finder/src/App.js
@@ -1,26 +1,19 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Component } from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+	render() {
+		const name = "John Doe";
+		const loading = false;
+		const showName = true;
+
+		return (
+			<div className="App">
+				{loading ? <h4>Loading...</h4> : <h1>Hello {showName && name}</h1>}
+				{/* <h1>Hello {(loading) ? "Loading..." : name}</h1> */}
+			</div>
+		);
+	}
 }
 
 export default App;
