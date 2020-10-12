46d3b8f04e3c7d4a2baace63ac12cb8866cb9323
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-13 00:48:47 +0530

    n/a

--- ReactFrontToBack/github-finder/src/components/users/UserItem.js
+++ ReactFrontToBack/github-finder/src/components/users/UserItem.js
@@ -1,25 +1,26 @@
-import React, { Component } from 'react'
+import React from 'react';
+import PropTypes from 'prop-types';
 
-class userItem extends Component {
-	render() {
-		const { login, avatar_url, html_url } = this.props.user;
+const UserItem = ({user: { login, avatar_url, html_url }}) =>  {
+	return (
+		<div className="card text-center">
+			<img
+				src={avatar_url}
+				alt=""
+				className="round-img"
+				style={{ width: "60px" }}
+			/>
+			<h3>{login}</h3>
 
-		return (
-			<div className="card text-center">
-				<img
-					src={avatar_url}
-					alt=""
-					className="round-img"
-					style={{ width: "60px" }}
-				/>
-				<h3>{login}</h3>
-
-				<div>
-					<a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
-				</div>
+			<div>
+				<a href={html_url} className="btn btn-dark btn-sm my-1">More</a>
 			</div>
-		)
-	}
+		</div>
+	)
 }
 
-export default userItem
+UserItem.propTypes = {
+	user: PropTypes.object.isRequired,
+}
+
+export default UserItem
