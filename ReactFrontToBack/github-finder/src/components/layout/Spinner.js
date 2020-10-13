f94d0d906897f00197e2b43b2b32cd95554040ba
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-13 13:09:19 +0530

    n/a

--- ReactFrontToBack/github-finder/src/components/layout/Spinner.js
+++ ReactFrontToBack/github-finder/src/components/layout/Spinner.js
@@ -1,9 +1,8 @@
-import React from 'react'
+import React, {Fragment} from 'react';
+import spinner from "./spinner.gif"
 
-export const Spinner = () => {
-	return (
-		<div>
-			
-		</div>
-	)
-}
+const Spinner = () => <Fragment>
+			<img src={spinner} alt="Loading..." style={{ width:"200px", margin:"auto", display:"block" }}/>
+		</Fragment>
+
+export default Spinner;
