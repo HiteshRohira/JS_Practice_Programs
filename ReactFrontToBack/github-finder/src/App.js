a35982e6c2404d8dd8179531b74b7d71d97ca69b
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-14 12:26:00 +0530

    n/a

--- ReactFrontToBack/github-finder/src/App.js
+++ ReactFrontToBack/github-finder/src/App.js
@@ -1,23 +1,23 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 import Navbar from "./components/layout/Navbar";
 import Users from "./components/users/Users";
 import Search from "./components/users/Search";
-import Alert from "./components/layout/Alert"
+import Alert from "./components/layout/Alert";
 import axios from "axios";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
 	state = {
 		users: [],
 		loading: false,
 		alert: null,
-	}
+	};
 
 	// async componentDidMount() {
 	// 	this.setState({ loading: true });
 
 	// 	const res = await axios.get(`https://api.github.com/users?client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-	
+
 	// 	this.setState({ users: res.data, loading: false });
 	// }
 
@@ -25,30 +25,32 @@ class App extends Component {
 	searchUsers = async (text) => {
 		this.setState({ loading: true });
 
-		const res = await axios.get(`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`);
-	
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
 		this.setState({ users: res.data.items, loading: false });
-	}
+	};
 
 	// clear Users from state
 	clearUsers = () => this.setState({ users: [], loading: false });
 
 	// Set alert
 	setAlert = (msg, type) => {
-		this.setState({alert: { msg, type }});
+		this.setState({ alert: { msg, type } });
 		setTimeout(() => {
-			this.setState({alert:null});
-		}, 4000)
-	}
+			this.setState({ alert: null });
+		}, 4000);
+	};
 
 	render() {
-		const {loading, users} = this.state;
+		const { loading, users } = this.state;
 
 		return (
 			<div className="App">
 				<Navbar />
 				<div className="container">
-					<Alert alert={this.state.alert}/>
+					<Alert alert={this.state.alert} />
 					<Search
 						searchUsers={this.searchUsers}
 						clearUsers={this.clearUsers}
