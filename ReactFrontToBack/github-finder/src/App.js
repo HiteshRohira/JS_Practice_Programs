c67f69834decddaa9573a88f963634ac60721b9c
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-16 01:13:09 +0530

    n/a

--- ReactFrontToBack/github-finder/src/App.js
+++ ReactFrontToBack/github-finder/src/App.js
@@ -27,18 +27,6 @@ const App = () => {
 	// 	this.setState({ users: res.data, loading: false });
 	// }
 
-	// Search Github Users
-	const searchUsers = async (text) => {
-		setLoading(true);
-
-		const res = await axios.get(
-			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
-		);
-
-		setUsers(res.data.items);
-		setLoading(false);
-	};
-
 	// Get single Github User
 	const getUser = async (username) => {
 		// componentDidMount passes value of username as login parameter here
@@ -90,7 +78,6 @@ const App = () => {
 								render={(props) => (
 									<Fragment>
 										<Search
-											searchUsers={searchUsers}
 											clearUsers={clearUsers}
 											showClear={users.length > 0 ? true : false}
 											setAlert={showAlert}
--- ReactFrontToBack/github-finder/src/context/github/GithubState.js
+++ ReactFrontToBack/github-finder/src/context/github/GithubState.js
@@ -21,6 +21,18 @@ const GithubState = (props) => {
 	const [state, dispatch] = useReducer(GithubReducer, initialState);
 
 	// Search Users
+	const searchUsers = async (text) => {
+		setLoading();
+
+		const res = await axios.get(
+			`https://api.github.com/search/users?q=${text}&client_id=${process.env.REACT_APP_GITHUB_CLIENT_ID}&client_secret=${process.env.REACT_APP_GITHUB_CLIENT_SECRET}`
+		);
+
+		dispatch({
+			type: SEARCH_USERS,
+			payload: res.data.items,
+		});
+	};
 
 	// Get Users
 
@@ -29,6 +41,7 @@ const GithubState = (props) => {
 	// Clear Users
 
 	// Set Loading
+	const setLoading = () => dispatch({ type: SET_LOADING });
 
 	return (
 		<GithubContext.Provider
@@ -37,6 +50,7 @@ const GithubState = (props) => {
 				user: state.user,
 				repos: state.repos,
 				loading: state.loading,
+				searchUsers,
 			}}>
 			{props.children}
 		</GithubContext.Provider>
