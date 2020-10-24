be1ff251e0766b938ce513de9355eb0c46d4a11c
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-24 14:10:41 +0530

    Moving on with the React Project

--- ReactFrontToBack/contact-keeper/client/src/components/context/contact/contactReducer.js
+++ ReactFrontToBack/contact-keeper/client/src/components/context/contact/contactReducer.js
@@ -1,4 +1,5 @@
 import {
+	GET_CONTACTS,
 	ADD_CONTACT,
 	DELETE_CONTACT,
 	SET_CURRENT,
@@ -6,14 +7,22 @@ import {
 	UPDATE_CONTACT,
 	FILTER_CONTACTS,
 	CLEAR_FILTER,
+	CONTACT_ERROR,
 } from "../types";
 
 export default (state, action) => {
 	switch (action.type) {
+		case GET_CONTACTS:
+			return {
+				...state,
+				contacts: action.payload,
+				loading: false,
+			};
 		case ADD_CONTACT:
 			return {
 				...state,
 				contacts: [...state.contacts, action.payload],
+				loading: false,
 			};
 		case UPDATE_CONTACT:
 			return {
@@ -21,6 +30,7 @@ export default (state, action) => {
 				contacts: state.contacts.map((contact) =>
 					contact.id === action.payload.id ? action.payload : contact
 				),
+				loading: false,
 			};
 		case DELETE_CONTACT:
 			return {
@@ -28,6 +38,7 @@ export default (state, action) => {
 				contacts: state.contacts.filter(
 					(contact) => contact.id !== action.payload
 				),
+				loading: false,
 			};
 		case SET_CURRENT:
 			return {
@@ -51,6 +62,11 @@ export default (state, action) => {
 			return {
 				...state,
 				filtered: null,
+			};
+		case CONTACT_ERROR:
+			return {
+				...state,
+				error: action.payload,
 			};
 		default:
 			return state;
--- ReactFrontToBack/contact-keeper/client/src/components/pages/Home.js
+++ ReactFrontToBack/contact-keeper/client/src/components/pages/Home.js
@@ -1,6 +1,6 @@
 import React, { useContext, useEffect } from "react";
 import Contacts from "../contacts/Contacts";
-import ContactForm from "../context/contact/ContactForm";
+import ContactForm from "../contacts/ContactForm";
 import ContactFilter from "../contacts/ContactFilter";
 import AuthContext from "../context/auth/authContext";
 
