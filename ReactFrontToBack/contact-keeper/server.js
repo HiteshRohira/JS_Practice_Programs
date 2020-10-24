7e4e635d56d8acfa40db95f5b33c99f22a64d19a
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-25 00:13:22 +0530

    Moving on with the React Project

--- ReactFrontToBack/contact-keeper/server.js
+++ ReactFrontToBack/contact-keeper/server.js
@@ -10,9 +10,9 @@ connectDB();
 // Init Middleware
 app.use(express.json({ extended: false }));
 
-app.get("/", (req, res) =>
-	res.json({ msg: "Welcome to the ContactKeeper API." })
-);
+// app.get("/", (req, res) =>
+// 	res.json({ msg: "Welcome to the ContactKeeper API." })
+// );
 
 // Define routes
 app.use("/api/users", require("./routes/users"));
