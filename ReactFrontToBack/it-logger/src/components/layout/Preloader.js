23f1068ff448be816c3032c9dcfcd4b77dc19a73
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-26 14:34:07 +0530

    Moving on with the React & Redux Project

---
+++ ReactFrontToBack/it-logger/src/components/layout/Preloader.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Preloader = () => {
+	return (
+		<div>
+			<div className="progress blue lighten-4">
+				<div className="indeterminate blue"></div>
+			</div>
+		</div>
+	);
+};
+
+export default Preloader;
