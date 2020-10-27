8add592da77992c14639641dc146fe8d66ac2ced
Hitesh Rohira <hiteshrohira15@gmail.com>
2020-10-27 13:51:55 +0530

    Moving on with the React & Redux Project

--- ReactFrontToBack/it-logger/src/actions/logActions.js
+++ ReactFrontToBack/it-logger/src/actions/logActions.js
@@ -35,15 +35,21 @@ export const getLogs = () => async (dispatch) => {
 };
 
 // Add new logs
-export const getLogs = () => async (dispatch) => {
+export const addLog = (log) => async (dispatch) => {
 	try {
 		setLoading();
 
-		const res = await fetch("/logs");
+		const res = await fetch("/logs", {
+			method: "POST",
+			body: JSON.stringify(log),
+			headers: {
+				"Content-Type": "application/json",
+			},
+		});
 		const data = await res.json();
 
 		dispatch({
-			type: GET_LOGS,
+			type: ADD_LOG,
 			payload: data,
 		});
 	} catch (err) {
