8349216d30718c9b8e20cd3c53bab30ecbdb7a2d
HiteshRohira <bop@pop-os.localdomain>
2020-12-21 14:25:16 +0530

    Done with Multiple Context

--- netninja-context-hooks-tutorial/context-app/src/App.js
+++ netninja-context-hooks-tutorial/context-app/src/App.js
@@ -2,15 +2,18 @@ import React from "react";
 import BookList from "./components/BookList";
 import Navbar from "./components/Navbar";
 import ThemeToggle from "./components/ThemeToggle";
+import AuthContextProvider from "./context/AuthContext";
 import ThemeContextProvider from "./context/ThemeContext";
 
 function App() {
 	return (
 		<div className="App">
 			<ThemeContextProvider>
-				<Navbar />
-				<BookList />
-				<ThemeToggle />
+				<AuthContextProvider>
+					<Navbar />
+					<BookList />
+					<ThemeToggle />
+				</AuthContextProvider>
 			</ThemeContextProvider>
 		</div>
 	);
--- netninja-context-hooks-tutorial/context-app/src/components/Navbar.js
+++ netninja-context-hooks-tutorial/context-app/src/components/Navbar.js
@@ -1,26 +1,40 @@
 import React, { Component } from "react";
+import { AuthContext } from "../context/AuthContext";
 import { ThemeContext } from "../context/ThemeContext";
 
 class Navbar extends Component {
 	render() {
 		return (
-			<ThemeContext.Consumer>
-				{(context) => {
-					const { isLightTheme, light, dark } = context;
-					const currentTheme = isLightTheme ? light : dark;
+			<AuthContext.Consumer>
+				{(authContext) => {
+					const { isAuthenticated, toggleAuth } = authContext;
 					return (
-						<nav
-							style={{ background: currentTheme.ui, color: currentTheme.text }}>
-							<h1>Context App</h1>
-							<ul>
-								<li>Home</li>
-								<li>About</li>
-								<li>Contact</li>
-							</ul>
-						</nav>
+						<ThemeContext.Consumer>
+							{(themeContext) => {
+								const { isLightTheme, light, dark } = themeContext;
+								const currentTheme = isLightTheme ? light : dark;
+								return (
+									<nav
+										style={{
+											background: currentTheme.ui,
+											color: currentTheme.text,
+										}}>
+										<h1>Context App</h1>
+										<div onClick={toggleAuth}>
+											{isAuthenticated ? "Logged In" : "Logged Out"}
+										</div>
+										<ul>
+											<li>Home</li>
+											<li>About</li>
+											<li>Contact</li>
+										</ul>
+									</nav>
+								);
+							}}
+						</ThemeContext.Consumer>
 					);
 				}}
-			</ThemeContext.Consumer>
+			</AuthContext.Consumer>
 		);
 	}
 }
